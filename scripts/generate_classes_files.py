dc137b10d537a1b73ec8941526ae922d081a5de0
Eric Werner <ewerybody+github@gmail.com>
2019-05-28 14:28:56 +0200

    added: lang switcher, embedd lang, colors, areas

--- scripts/generate_classes_files.py
+++ scripts/generate_classes_files.py
@@ -3,14 +3,17 @@ import json
 from pprint import pprint
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-LANGS = ('de', 'en')
+LANGS = {'de': 'Deutsch',
+         'en': 'English'}
 MD_HEADER = (
 """---
 title: '{title}'
 taxonomy:
     category:
         - calendar
+visible: false
 ---\n\n""")
+SITE = 'https://rmswing.de/{lang}/classes-2'
 
 
 def main():
@@ -26,11 +29,24 @@ def main():
 
 def create_md(data_dir, lang, data, area):
     content = MD_HEADER.format(title=data['title'][lang])
-    content += f'<iframe src="{data["embed_template"]}" style="border-width:0" width="100%" height="800" frameborder="0" scrolling="no"></iframe>'
 
+    # iframe contents
+    content += f'<iframe src="{data["embed_template"]}" style="border-width:0" width="100%" height="800" frameborder="0" scrolling="no"></iframe>\n\n'
     # gather calendar ids and insert into iframe src string
     ids = [source['embed_id'] for source in data['sources'].values()]
-    content = content.format(sources=data['sources_sep'].join(ids))
+    colors = [data['color_tmp'].format(hex=source['color']) for source in data['sources'].values()]
+    content = content.format(
+        sources=data['sources_sep'].join(ids),
+        colors=''.join(colors),
+        lang=lang)
+    
+    # custom lang switcher
+    for key, lang_name in LANGS.items():
+        if key == lang:
+            content += f' [**{lang_name}**]({SITE.format(lang=key)})'
+        else:
+            content += f' [{lang_name}]({SITE.format(lang=key)})'
+    content += '<br>'
 
     # assemble are shorthands
     areas_in_use = set(source['area'] for source in data['sources'].values())
@@ -48,8 +64,15 @@ def create_md(data_dir, lang, data, area):
     # calendar source summary
     subcals = []
     for name, source_nfo in data['sources'].items():
-        calurl = data['direct_url_template'].format(short_id=source_nfo['short_id'])
-        subcals.append(f'* [{name}]({calurl})')
+        cal_id = source_nfo['short_id']
+        if '@' not in cal_id:
+            cal_id = data['calendar_id_template'].format(short_id=cal_id)
+        calurl = data['direct_url_template'].format(cal_id=cal_id)
+        
+        subcal_entry = f'* [{name}]({calurl})'
+        if 'area' in source_nfo:
+            subcal_entry += ' - ' + source_nfo['area']
+        subcals.append(subcal_entry)
     content += '\n\n{title}:\n{subcals}'.format(
         title=data['sub_calendar_title'][lang],
         subcals='\n'.join(subcals))
